1b84fdc253da8194ba97d43adb75c6e2d206037a
Shivansh Saxena <i.shivanshhh@gmail.com>
2026-09-10 18:40:33 +0530

    done with functinality of register form

---
+++ (auth)/register/page.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { useAuthStore } from "@/store/Auth";
+
+
+function RegisterPage(){
+    const { createAccount, login } = useAuthStore();
+    const [isLoading , setIsLoading] = React.useState(false);
+    const [error , setError] = React.useState("");
+
+    const handleSubmit = async(e : React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        // collection of data 
+        const formData = new FormData(e.currentTarget)
+        const firstname = formData.get("firstname")
+        const lastname = formData.get("lastname")
+        const email = formData.get("email")
+        const password = formData.get("password")
+
+        // validate 
+
+        if(!firstname || !lastname || !email || !password){
+            setError(() => "Please fill out all the required fields")
+            return 
+        }
+
+        // call the store
+
+        setIsLoading(true)
+        setError("")
+
+        const response = await createAccount(
+            `${firstname} ${lastname}`,
+            email?.toString(),
+            password?.toString()
+        )
+
+        if(response.error){
+            setError(() => response.error!.message)
+        } else {
+            const loginResponse = await login(email.toString() , password.toString())
+
+            if(loginResponse.error){
+                setError(() => loginResponse.error!.message)
+            }
+        }
+
+        setIsLoading(() => false)
+
+        
+    }
+
+
+
+    return (
+        <div>Register Page</div>
+    )
+}
+
+export default RegisterPage;
+
+
